3839464b38451d18966364d99ef214932d774815
Prathmesh <prathmeshagrawal2000@gmail.com>
2021-06-05 11:58:34 +0530

    update

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://www.instagram.com/p/CAGCZMejx91/?utm_source=ig_web_copy_link",
+    "name": "Prathmesh Agrawal",
+    "role": "Full Stack MERN Developer, Java and CPP programmer",
+    "linkedinId":"https://www.linkedin.com/in/prathmesh-agrawal-961932156/",
+    "gmailId":"prathmeshagrawal2000@gmail.com",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/prathmesh-agrawal-961932156/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Prathmesh2000",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"An aspiring technical student currently pursuing my B.tech, having good commands over various programming languages like c++, java, python, and Data structures. Who loves to make UI for both web and android application strong command over CSS(pure), react JS, having good Skilled in public speaking, interpersonal skills along technical skills. Holding experience in both leading and working in a group on various occasions. I am dedicated towards learning new technologies.",
+    "address":"Gwalior, Madhya Pradesh, India",
+    "education":[
+      {
+        "UniversityName":"Lovely Professional University, Punjab",
+        "specialization":"B.Tech (CSE)",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2022",
+        "Achievements":'1st in "Decorous 2.0- Let us take the work out of your fun!" organized by School of CSE, LPU'
+      },
+      {
+        "UniversityName":"Kiddys Corner School, Gwalior",
+        "specialization":"Senior Secondary: PCM",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2018",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"The Radiant School, Gwalior",
+        "specialization":"Higher Secondary",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2016",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Skills: ",
+    "skills":[
+      {
+        "skillname":"Cpp"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"DSA"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"VanilaJs"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"AndroidStudio"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"To Do List",
+        "description":"To Do List on Live Server made using ReactJS https://todolist-prathmesh.herokuapp.com/",
+        "imgurl":"images/portfolio/toDoList.JPG"
+      },
+      {
+        "name":"Blog app",
+        "description":"Blogging website build using NodeJS",
+        "imgurl":"images/portfolio/blog.jpg"
+      },
+      {
+        "name":"Covid Case tracker",
+        "description":"Live covid cases tracker using reactJS and Rest API",
+        "imgurl":"images/portfolio/cct.jpg"
+      },
+      {
+        "name":"Chat APP (Buddy)",
+        "description":"Real time chat Application using java, firebase.",
+        "imgurl":"images/portfolio/chatapp.jpg"
+      },
+    ]
+  }
+  
+  export default resumeData
